0df26f551ac89c77c514ab45da0a2921143d1287
Sasha <sasha@sasha.com>
2021-04-29 15:27:56 +0000

    BSF

---
+++ knight.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+from typing import List, Tuple
+
+
+def get_parents(tree):
+    reversed_keys = [key for key in tree][::-1]
+    new_tree = {}
+    parents = []
+    for key in reversed_keys:
+        new_tree[key] = tree[key]
+    for key in new_tree:
+        if not parents:
+            parents.extend([new_tree[key][-1], key])
+            continue
+        if parents[-1] in new_tree[key]:
+            parents.append(key)
+    return parents[::-1]
+
+def get_available_moves(cell, axis_Y):
+    all_possible_moves = [(-2, 1), (-1, 2), (1, 2), (2, 1), (2, -1), (1, -2), (-1, -2), (-2, -1)]
+    available_moves = []
+    for move in all_possible_moves:
+        if 0 < cell[1] + move[0] <= 8 \
+        and 0 < axis_Y.index(cell[0]) + move[1] <=7:
+            available_moves.append(move)
+    return available_moves
+
+def shortest_path(start: tuple = ('c', 3), target: tuple = ('d', 8), board_width: int = 8, board_height: int = 8) -> List[Tuple[str, int]]:
+    if start == target:
+        return start
+    queue = [start]
+    axis_Y = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+    visited_cells = []
+    moves_tree = {}
+
+    while queue:
+        current_cell = queue.pop(0)
+        available_moves = get_available_moves(current_cell, axis_Y)
+        
+        for move in available_moves:
+            chess_board_cell = (axis_Y[axis_Y.index(current_cell[0]) + move[1]], move[0] + current_cell[1])
+            if chess_board_cell in visited_cells:
+                continue
+            if current_cell not in moves_tree:
+                moves_tree[current_cell] = []
+            moves_tree[current_cell].append(chess_board_cell)
+            if chess_board_cell == target:
+                return get_parents(moves_tree)
+            visited_cells.append(chess_board_cell)
+            queue.append(chess_board_cell)
+
+
+if __name__ == '__main__':
+    path = shortest_path()
+    print(path)
